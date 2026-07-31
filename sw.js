2c2603827262abe0dc5c90bf329735d99f786c06
madeyu97 <mattjgibb.97@gmail.com>
2026-07-31 09:46:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jingxue-v18';
+const CACHE = 'jingxue-v19';
 const ASSETS = [
   './', './index.html', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './icon-maskable.png', './apple-touch-icon.png'
