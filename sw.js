698f2dbf3887c70d02ab525277d29b9e7dccf483
madeyu97 <mattjgibb.97@gmail.com>
2026-09-17 11:39:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'jingxue-v73';
+
+const CACHE = 'jingxue-v75';
 const ASSETS = [
   './', './index.html', './config.js', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './icon-maskable.png', './apple-touch-icon.png'
@@ -24,4 +25,3 @@ self.addEventListener('fetch', e => {
     }).catch(() => req.mode === 'navigate' ? caches.match('./index.html') : Promise.reject(new Error('offline'))))
   );
 });
- 
