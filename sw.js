c34935c221e9b24df973c520c20a1638ee0f0055
madeyu97 <mattjgibb.97@gmail.com>
2026-09-17 12:09:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE = 'jingxue-v75';
+const CACHE = 'jingxue-v76';
 const ASSETS = [
   './', './index.html', './config.js', './manifest.webmanifest',
   './icon-192.png', './icon-512.png', './icon-maskable.png', './apple-touch-icon.png'
